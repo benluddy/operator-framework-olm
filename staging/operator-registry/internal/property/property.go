njhale/operator-registry

feat(bundle): consume explicit properties
Define a new configuration file that can be provided by bundle authors to explicitly declare properties.
The file itself can have any name, but must both exist in the top-level metadata directory of the bundle
and consist of YAML with a top-level properties element containing an array of type/value tuples.

Signed-off-by: Nick Hale <njohnhale@gmail.com>

internal/property/property.go
@@ -30,6 +30,10 @@ func (p Property) Validate() error {
 	return nil
 }
 
+func (p Property) String() string {
+	return fmt.Sprintf("type: %s, value: %s", p.Type, p.Value)
+}
+
 type Package struct {
 	PackageName string `json:"packageName"`
 	Version     string `json:"version"`

benluddy/operator-framework-olm

Upstream-repository: operator-registry
Upstream-commit: d79f6daa1325084bc90adb89aa03897d4d753faa

staging/operator-registry/internal/property/property.go
@@ -30,6 +30,10 @@ func (p Property) Validate() error {
 	return nil
 }
 
+func (p Property) String() string {
+	return fmt.Sprintf("type: %q, value: %q", p.Type, p.Value)
+}
+
 type Package struct {
 	PackageName string `json:"packageName"`
 	Version     string `json:"version"`